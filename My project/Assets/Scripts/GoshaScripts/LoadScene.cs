f5dfafb4fe4edb82464c9bf17645a7c020a1f752
ThatOneDude0 <99406502+ThatOneDude0@users.noreply.github.com>
2023-04-29 16:41:51 +0300

    LoadScene
    LoadScene

---
+++ My project/Assets/Scripts/GoshaScripts/LoadScene.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+    [SerializeField] private int _sceneIndex;
+    [SerializeField] private Slider _sliderProgress;
+
+    public void Start()
+    {
+        StartCoroutine(LoadAsync());
+    }
+
+    IEnumerator LoadAsync()
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(_sceneIndex);
+
+        while (!operation.isDone)
+        {
+            float prgress = operation.progress / 5.0f;
+            _sliderProgress.value = prgress;
+            yield return null;
+        }
+    }
+}
