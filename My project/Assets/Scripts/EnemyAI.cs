3deba51519ab7c6e3bbc916a12ce168f5372e974
Nikitazaborrskikh <zaborskikh03@gmail.com>
2023-04-29 17:53:20 +0300

    Added enemyMovement

---
+++ My project/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    
+    public float moveSpeed = 5f; 
+    public float aggroRange = 5f; 
+    
+    private Transform target; 
+    private bool isChasing = false; 
+    
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+    
+    void Update()
+    {
+        
+        float distanceToTarget = Vector2.Distance(transform.position, target.position);
+        
+        
+        if (distanceToTarget < aggroRange)
+        {
+            isChasing = true; 
+        }
+        else
+        {
+            isChasing = false; 
+        }
+        
+        if (isChasing)
+        {
+            
+            transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+        }
+    }
+}
+
