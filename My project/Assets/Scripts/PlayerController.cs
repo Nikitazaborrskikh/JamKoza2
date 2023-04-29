86792195a82031b2d9961ff2be3e9c6135a27bc5
Nikitazaborrskikh <zaborskikh03@gmail.com>
2023-04-29 16:08:19 +0300

    Created

---
+++ My project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f; 
+    private Rigidbody2D playerRigidbody2D; 
+    private Vector2 direction; 
+
+    void Start()
+    {
+        playerRigidbody2D = GetComponent<Rigidbody2D>(); 
+    }
+
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal"); 
+        float y = Input.GetAxisRaw("Vertical"); 
+        direction = new Vector2(x, y).normalized; 
+
+        if (direction.magnitude > 0) 
+        {
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg; 
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward); 
+        }
+    }
+
+    void FixedUpdate()
+    {
+        playerRigidbody2D.MovePosition(playerRigidbody2D.position + direction * speed * Time.fixedDeltaTime); 
+    }
+}
